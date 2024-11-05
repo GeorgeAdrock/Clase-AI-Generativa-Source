09919e629c6d2323be4660f46a19934306938ac9
George <jorge.araya.vergara@gmail.com>
2024-11-04 23:27:03 -0300

    Update UnirseGrupo.py

--- Curso/Trabajo 1/src/UnirseGrupo.py
+++ Curso/Trabajo 1/src/UnirseGrupo.py
@@ -7,7 +7,7 @@ payload = {
     "name": "Jorge Araya Vergara"
 }
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvcmdlLmFyYXlhLnZlcmdhcmFAZ21haWwuY29tIiwiaWF0IjoxNzMwNjc3Njk4LCJleHAiOjE3MzA2ODEyOTh9.S6R8aVoSCH1VmF9VQes1zhaER9hQ0_D9XeiSoQ51Jqc",
+    "Authorization": "Bearer ",
     "content-type": "application/json"
 }
 
