64e661d87d3fbcfe3962b715911c490d959db75d
George <jorge.araya.vergara@gmail.com>
2024-11-04 23:26:28 -0300

    Update CrearGrupo.py

--- Curso/Trabajo 1/src/CrearGrupo.py
+++ Curso/Trabajo 1/src/CrearGrupo.py
@@ -4,7 +4,7 @@ url = "https://jhhqwn3twg.execute-api.us-east-1.amazonaws.com/prod/groups"
 
 payload = { "name": "Grupo para ejercicio " }
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvcmdlLmFyYXlhLnZlcmdhcmFAZ21haWwuY29tIiwiaWF0IjoxNzMwMjQ3MjY0LCJleHAiOjE3MzAyNTA4NjR9.G1H77-JTACl2T8o19jimKHu9UUNmGP5FNlNHhNS87no",
+    "Authorization": "Bearer ",
     "content-type": "application/json"
 }
 
